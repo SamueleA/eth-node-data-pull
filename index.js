e117725d43ecfaf6192fc07d9efcd77ebd1a67cd
safe user <batonenbois@gmail.com>
2018-05-12 19:52:44 +0000

    removed getbalance

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var Web3 = require('web3');
 
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-var contractAddress = '0x2a0c0dbecc7e4d658f48e01e3fa353f44050c208';
-
 var contractsArray = ['0xb1690c08e213a35ed9bab7b318de14420fb57d8c','0xc7af99fe5513eb6710e6d5f44f9989da40f27f26','0x06012c8cf97bead5deae237070f9587f8e7a266d'];
 
 
@@ -13,8 +11,7 @@ update();
 
 async function update() {
 	try{	
-	var blockAmount = 43 //block time of 14s.  10 minutes interval = > 42.8 blocks
-	let getBalancePromise = web3.eth.getBalance(contractAddress);		
+	var blockAmount = 5//43 //block time of 14s.  10 minutes interval = > 42.8 blocks		
 	let latestBlock = await web3.eth.getBlock('latest', true);
 	let latestBlockNumber = latestBlock.number;
 	let blocksDataPromises=[];
