441f84d713be0084a4136b444ec836bbcf5b941c
safe user <batonenbois@gmail.com>
2018-05-12 23:04:14 +0000

    completed

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var Web3 = require('web3');
+var fs = require('fs');
+var moment = require('moment');
 
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 var contractsArray = ['0xb1690c08e213a35ed9bab7b318de14420fb57d8c','0xc7af99fe5513eb6710e6d5f44f9989da40f27f26','0x06012c8cf97bead5deae237070f9587f8e7a266d'];
 
+var now =  moment();
+var formattedDate = now.format('YYYY-MM-DD HH:mm:ss Z');
+ fs.appendFile('ethNodePollingLog.txt', '\nProgram started. Time UTC is : ' + formattedDate + '. Block polling every 10 minutes' , (err)=>{
+ 	if (err) throw err;
+ 	console.log('file updated');
+ });
 
-update();
 
-//setInterval(update, 20000);
+setInterval(update, 600000);
 
 async function update() {
 	try{	
@@ -47,6 +54,10 @@ async function update() {
 		}
 	
 		console.log('volume is ' + volume, 'transaction count is ' + transactionCount);
+		fs.appendFile('ethNodePollingLog.txt', '\nvolume is ' + volume + ' | transaction count is ' + transactionCount, (err)=>{
+			if (err) throw err;
+			console.log('file updated');
+		});
 			
 	} catch(e){ 
 		console.log(e);	
